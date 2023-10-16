9394eb530f17f73c880f6ff4062dcaaff7e366c0
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2023-10-17 01:10:00 +0900

    firebaseにあるデータを取得して表示することはできた

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import db from './firebase'
+import { Timestamp, collection, getDocs } from 'firebase/firestore'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Posts {
+  title: string
+  text: string
+  timeStamp: Timestamp
 }
 
-export default App;
+function App() {
+  const [posts, setPosts] = useState<Posts[]>([])
+
+  useEffect(() => {
+    // データベースからデータを取得する。
+    const postData = collection(db, 'posts')
+    getDocs(postData).then((snapShot) => {
+      // console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      const postArray: Posts[] = snapShot.docs.map((doc) => doc.data() as Posts) // データ型をPostに変換
+      setPosts(postArray)
+    })
+  }, [])
+
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="App">
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <h2>{post.title}</h2>
+            <p>{post.text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
